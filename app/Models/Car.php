efb043ad4f703d07f1132b698eb61c715eb37379
Shamble94 <fratus.nicholas@gmail.com>
2024-02-29 12:12:50 +0100

    Seeder tabella optional

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Optional;
 
 class Car extends Model
 {
     use HasFactory;
 
     protected $fillable = ["marca", "modello","alimentazione","prezzo","num_porte","colore","anno","chilometri"];
+
+    public function optionals() {
+        return $this->hasMany(Optional::class);
+    }
+
 }
