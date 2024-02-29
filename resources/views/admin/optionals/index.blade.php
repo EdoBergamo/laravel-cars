f9dd2bcd4b0a9f5a9a0881a40f3449409f78a6f0
nicolocamassa <camassanicolo03@gmail.com>
2024-02-29 15:57:37 +0100

    [UPDATE] add modify button

--- resources/views/admin/optionals/index.blade.php
+++ resources/views/admin/optionals/index.blade.php
@@ -10,7 +10,7 @@
                     </div>
 
                     <div>
-                        <a href=" {{ route("admin.optionals.create")}} "><button class="btn btn-primary">Add New Car</button></a>
+                        <a href=" {{ route("admin.optionals.create")}} "><button class="btn btn-primary">Add New Optional</button></a>
                     </div>    
                </div>
             </div>
@@ -32,6 +32,11 @@
                                 <td>{{ $optional->name}}</td>
                                 <td>{{ $optional->price}}</td>
                                 <td>{{ $optional->type}}</td>
+                                <td>
+                                    <a href=" {{ route("admin.optionals.update", ['optional' => $optional->id])}} "><button class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
+                                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001"/>
+                                      </svg></button></a>
+                                </td>
                               {{--   <td>
                                     <a href="{{ route("admin.optionals.show", ["car" => $car->id ])}}"><button class="btn btn-sm btn-square btn-primary"><i class="fas fa-eye"></i></button></a>
                                     <a href="{{ route("admin.cars.edit", ["car" => $car->id ])}}"><button class="btn btn-sm btn-square btn-warning"><i class="fas fa-edit"></i></button></a>
